a5a018376803ce4bdbb03f0cceeea30f45abfbe8
dpavlev <dimitar.pavlev03@gmail.com>
2025-02-24 22:14:45 +0200

    Add current time on page

---
+++ index.js
@@ -0,0 +1,6 @@
+const h1 = document.querySelector("#currentTime");
+window.setInterval(refreshTime, 1000);
+function refreshTime() {
+    const current = new Date(Date.now());
+    h1.textContent = current.toLocaleTimeString("BG-bg");
+}
