2ea6103e1e6c80199cb5aeca1b1143ad63ff011b
dpavlev <dimitar.pavlev03@gmail.com>
2025-02-28 20:11:32 +0200

    Add new datetime function

--- src/addOptions.js
+++ src/addOptions.js
@@ -1,26 +1,30 @@
 export default function () {
-    const hourSelect = document.querySelector("#hour");
-    for (let i = 0; i < 25; i++) {
-        const option = document.createElement("option");
-        if (i / 10 < 1) {
-            option.value = "0" + i;
-            option.textContent = "0" + i;
-        } else {
-            option.value = i;
-            option.textContent = i;
+    const hourSelects = document.querySelectorAll("select.hour");
+    hourSelects.forEach((hourSelect) => {
+        for (let i = 0; i < 25; i++) {
+            const option = document.createElement("option");
+            if (i / 10 < 1) {
+                option.value = "0" + i;
+                option.textContent = "0" + i;
+            } else {
+                option.value = i;
+                option.textContent = i;
+            }
+            hourSelect.append(option);
         }
-        hourSelect.append(option);
-    }
-    const minutesSelect = document.querySelector("#minutes");
-    for (let i = 0; i < 60; i++) {
-        const option = document.createElement("option");
-        if (i / 10 < 1) {
-            option.value = "0" + i;
-            option.textContent = "0" + i;
-        } else {
-            option.value = i;
-            option.textContent = i;
+    });
+    const minutesSelects = document.querySelectorAll("select.minutes");
+    minutesSelects.forEach((minutesSelect) => {
+        for (let i = 0; i < 60; i++) {
+            const option = document.createElement("option");
+            if (i / 10 < 1) {
+                option.value = "0" + i;
+                option.textContent = "0" + i;
+            } else {
+                option.value = i;
+                option.textContent = i;
+            }
+            minutesSelect.append(option);
         }
-        minutesSelect.append(option);
-    }
+    });
 }
